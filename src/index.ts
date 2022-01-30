c782272779271d68fd70c0fd81edbca99e337627
Jawad Ali <alijamil734@gmail.com>
2022-01-30 11:43:05 +0500

    📦 NEW: add error handling

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import bodyParser from "body-parser"
+import "express-async-errors"
 import express from "express"
-import seeding from "./seeding";
-import router from "./router";
+import seeding from "./seeding"
+import router from "./router"
 import mongoose from "mongoose"
 require("dotenv").config()
 
@@ -20,6 +21,23 @@ mongoose.connect(dbURL, {
   // data seeding
   seeding()
   app.use(router)
+
+
+
+  // error handling
+  app.use("/", (req: any, res: any) => {
+    res.status(400).json({ error: "Requested path not found on server" })
+  })
+
+  app.use((err: any, req: any, res: any, next: any) => {
+    console.error(err)
+
+    err.statusCode = err.statusCode || 500
+    res.status(err.statusCode).json({
+      error: err.message || "Server Error"
+    })
+  })
+
   // start server after connection with db
   const port = process.env.PORT || 3000
   app.listen(port, () => {
